f4ce1c251d63b2610278d060ab26b28c6e14ea0a
Alexis-Dk <alexeydruzik@inbox.ru>
2019-02-22 11:40:33 +0300

    fix

--- src/lesson2/case6/Main.java
+++ src/lesson2/case6/Main.java
@@ -5,8 +5,9 @@ import java.util.List;
 
 public class Main {
 
-    static boolean find(List<? extends Product> all, Product p) {    //!!! --- if delete '? extends' - doesn't work
-        //static boolean find(Product> all, Product p) {
+    //static boolean find(List<? extends Product> all, Product p) {    //!!! --- if delete '? extends' - doesn't work
+        //static boolean find(List<Product> all, Product p) {
+        static <T extends Product> boolean find(List<T> all, T p) {
 
         for (Product sp: all) {
             if (sp.isSameProduct(p)) {
@@ -19,9 +20,16 @@ public class Main {
 
     public static void main(String[] args) {
 
-        List<Camera> cameras = new ArrayList();
-        find(cameras, new Camera());
+        List<Camera> cameras1 = new ArrayList();
+        find(cameras1, new Camera());
 
+        List<Camera> cameras2 = new ArrayList();
+        find(cameras2, new Phone());
+        find(cameras2, new Product());
+
+        List<Product> products = new ArrayList();
+        find(products, new Camera());
+        find(products, new Phone());
     }
 
 }
